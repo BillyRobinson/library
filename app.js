f0c620dc95564771dabacae453421b9cce00e9c8
billyrobinson <billyrobinson18@gmail.com>
2023-02-20 13:12:50 +0000

    middle

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+
+addBookSubmit.addEventListener("click", addBookToLibrary);
+
 let myLibrary = [];
 
-
-
+function Book(title, author, read){
+    this.title = title
+    this.author = author
+    this.read = read
+}
 function addBookToLibrary(){
 
+
+
+
+    console.log("clicksubmit")
 }
